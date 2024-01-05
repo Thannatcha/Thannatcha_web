bb0d340e41b85d79611e72081b3a97cf7ed0bfc2
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2024-01-05 10:53:44 +0700

    Update index.html and Create post.php
    Update index.html and Create post.php

---
+++ post.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard Thannatcha</h1>
+    <hr><br>
+    <div style="text-align: center;"> 
+    <?php
+    echo"ต้องการดูกระทู้หมายเลข $_GET[id] <br>"
+    ?>
+    <table style="border: 2px solid black; width: 40%;" align="center">
+    <tr><td colspan="2" style="background-color: #6cd2fe;">แสดงความคิดเห็น</td></tr>
+    <tr>
+        <td>
+            <form>
+                <textarea name="message" rows="20" cols="50"></textarea>
+            </form>
+        </td>
+    </tr>
+    <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+    </table>
+    <a href="index.html">กลับไปหน้าหลัก</>
+    </div>
+</body>
+</html>
