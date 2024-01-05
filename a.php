25618f2161797d7fdb01c7257d6e67ab5436af1b
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2024-01-05 08:47:07 +0700

    Ex2 in php1
    Ex2 in php1

---
+++ a.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 4;
+    $b = 10;
+    $c = ($b-5)*$a;
+
+    echo "Result 1 = ".$c ."<BR>";
+    echo 'Result 2 = '.$c.'<BR>';
+    echo "Result 3 = $c <BR>";
+    echo 'Result 4 = $c <BR>';
+    echo "Result 5 = ".($c + $b) ."<BR>";
+    ?>
+</body>
+</html>
