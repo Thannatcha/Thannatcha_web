14990c78292155448f26d5d61debea8d4f5bea3c
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2024-02-09 11:18:32 +0700

    bootstrap5.3.2
    bootstrap5.3.2

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <title>Webboard</title>
 </head>
 <body>
     <h1 style="text-align: center;">Webboard Thannatcha</h1>
