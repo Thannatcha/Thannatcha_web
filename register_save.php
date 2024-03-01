59f7a915c7a97ab8c60bca9d6afa1f1edf5eb37b
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2024-03-01 10:09:38 +0700

    Update register_save.php

--- register_save.php
+++ register_save.php
@@ -4,6 +4,7 @@ $password=$_POST['password'];
 $name=$_POST['name'];
 $gender=$_POST['gender'];
 $email=$_POST['email'];
+header("location:login.php");
 
 $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
 $sql="INSERT INTO user (login, password, name, gender, email, role)
