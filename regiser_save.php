d437ec598f69b200b077fab75f2aaa63693001f8
Thannatcha <152575189+Thannatcha@users.noreply.github.com>
2024-03-01 09:48:43 +0700

    Update register_save.php
    Update register_save.php

---
+++ regiser_save.php
@@ -0,0 +1,13 @@
+<?php
+$login=$_POST['login'];
+$password=$_POST['password'];
+$name=$_POST['name'];
+$gender=$_POST['gender'];
+$email=$_POST['email'];
+
+$conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+$sql="INSERT INTO user (login, password, name, gender, email, role)
+ VALUES('$login','$password','$name','$gender','$email','m',)";
+$conn->exec($sql);
+$conn=null;
+?>
